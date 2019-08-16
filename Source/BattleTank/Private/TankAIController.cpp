98c260e3c8da2a7df8a621afd72d8fd62f03a3f9
Buddhamon <wlacey@smu.edu>
2019-08-16 03:46:36 -0500

    Added an AIController Source File; Slightly updated the Map

---
+++ Source/BattleTank/Private/TankAIController.cpp
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TankAIController.h"
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+	ATank* ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AIController not possessing a Tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AIController possessing: %s"), *(ControlledTank->GetName()));
+	}
+
+}
+
+ATank* ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
--- Source/BattleTank/Public/TankAIController.h
+++ Source/BattleTank/Public/TankAIController.h
@@ -4,20 +4,20 @@
 
 #include "CoreMinimal.h"
 #include "Tank.h"
-#include "GameFramework/PlayerController.h"
-#include "TankPlayerController.generated.h"
+#include "AIController.h"
+#include "TankAIController.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class BATTLETANK_API ATankPlayerController : public APlayerController
+class BATTLETANK_API ATankAIController : public AAIController
 {
 	GENERATED_BODY()
 
 public:
 
 	ATank* GetControlledTank() const;
-    
-    virtual void BeginPlay() override;
+
+	virtual void BeginPlay() override;
 };
